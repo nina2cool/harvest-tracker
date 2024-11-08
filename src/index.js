3585999af27a1e23ef7e296242cc245399e38783
Christina Harder <christina.harder@inventive.io>
2024-11-07 23:08:04 -0600

    trying to fix bootstrap error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
